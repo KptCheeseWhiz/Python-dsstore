0eb342066973013bf18556ceac5aecbf5c2f10be
Vincent Laferrière <github@monordibogue.com>
2023-08-16 17:00:12 -0400

    added pyproject.toml and setup.cfg

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import dsstore
 import os
 import sys
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        sys.exit("Usage: python main.py <DS_STORE FILE>")
-    if not os.path.exists(sys.argv[1]):
-        sys.exit("File not found: Usage main.py <file>")
-    with open(sys.argv[1], "rb") as f:
-        d = dsstore.DS_Store(f.read(), debug=False)
-        files = d.traverse_root()
-        print("Count: ", len(files))
-        for f in files:
-            print(f)
+def main():
+    for file in sys.argv[1:]:
+      if not os.path.exists(file):
+        continue
+      with open(file, "rb") as f:
+          print(">", file)
+          d = dsstore.DS_Store(f.read(), debug=False)
+          files = d.traverse_root()
+          for f in files:
+              print(">>", f)
 
+if __name__ == "__main__":
+    main()
